c9538054f261e712bda2731b6c3bb7f85a251dc5
Antonio Carlos da Silva Senra Filho <acsenrafilho@gmail.com>
2016-03-17 14:57:26 +0000

    version 1.0 release Run phantom_deformation.sh script to perform all the process

--- multishell_resemp.m
+++ multishell_resemp.m
@@ -1,8 +1,8 @@
-function multishell_resemp(img_nii,bval,bvec,grad_dir_out,SH_order)
+function multishell_resemp(img_nii,bval,bvec,out_bval_size,grad_dir_out,SH_order)
 
 %function multishell_resemp(img_nii,bval,bvec,grad_dir_out,SH_order)
 %
-%Apply the Spherical Harmonics DWI resampling to reduce gradient order in DTI data. 
+%Apply the Spherical Harmonics DWI resampling to reduce gradient order in DTI data.
 %
 %img_nii       = Input data filename path (.nii)
 %bval          = Bval filename path (.bval)
@@ -14,20 +14,20 @@ function multishell_resemp(img_nii,bval,bvec,grad_dir_out,SH_order)
 [path, filename, ext] = fileparts(img_nii);
 
 % Creating the brain mask
-system(sprintf('bet %s %s_brain -f 0.1 -m',img_nii,strcat(path,'/',filename)));
+system(sprintf('bet %s brain -f 0.1 -m',img_nii));
 
 % untar the brain mask file and atribute brain_mask variable
-system(sprintf('gzip -d %s',strcat(path,'/',filename,'_brain_mask.nii.gz')));
-brain_mask=strcat(path,'/',filename,'_brain_mask.nii');
+system(sprintf('gzip -d brain_mask.nii.gz'));
+brain_mask=strcat(path,'/brain_mask.nii');
 
 % Remove unncessary files
-system(sprintf('rm %s',strcat(path,'/*_brain.nii.gz')));
+system(sprintf('rm brain.nii.gz'));
 
 %Determining the number of B0 in the DTI data
 numBvals=0;
 bval_list=load(bval);
 for i=1:length(bval_list)
-    if bval_list(i)<100        
+    if bval_list(i)<100
     numBvals=numBvals+1;
     end
 end
@@ -42,15 +42,20 @@ NBL_SH_resample(img_nii,brain_mask,bvec_values,numBvals,strcat(path,'/',filename
 
 % Save bvec and bval to the new resample data
 bval=load(bval);
-new_bval=zeros(1,grad_dir_out);
-for i=1:grad_dir_out
-   new_bval(i)=bval(i); 
+new_bval=zeros(1,out_bval_size+grad_dir_out);
+for i=1:length(new_bval)
+    if i <= out_bval_size
+       new_bval(i)=bval(i);
+    else
+       new_bval(i)=bval(i+12);
+    end
 end
 save(strcat(path,'/',filename,'_resamp.bval'),'new_bval','-ascii');
-new_bvec=ndirez(grad_dir_out);
+new_bvec=zeros(out_bval_size+grad_dir_out,3);
+b1values=ndirez(grad_dir_out);
+new_bvec(out_bval_size+1:length(new_bvec),:)=b1values;
 new_bvec=new_bvec';
 save(strcat(path,'/',filename,'_resamp.bvec'),'new_bvec','-ascii');
 
 % Clear the variables
 % delete bvec bvec_values bval
-
--- phantom_deformation.sh
+++ phantom_deformation.sh
@@ -1,23 +1,71 @@
 #!/bin/bash
 # Script to split the DTI volume and apply a deformation motion artifact from a .mat file (eddy_correct)
 
-INPUT_IMG=$1
-MATRIX=$2
-RESOLUTION=$3
+usage(){
+  echo "  Script to apply a eddy current distortion in a DTI data."
+  echo "  Usage: phantom_deformation <Input image> <Bval file> <Bvec file> <Distortion matrix> <Number of B0> <Spatial resolution (mm)>"
+  echo ""
+  echo "   Input image          = The DTI volume which you want to apply eddy current distortions."
+  echo "   Bval file            = Bval file from the input image (.bval)."
+  echo "   Bvec file            = Bvec file from the input image (.bvec)."
+  echo "   Distortion matrix    = A textfile which informs a 4x4 affine matrix in order to apply in each volume of the DTI input data. Tip: Use FSL-eddy-correct ecclog file."
+  echo "   Number of B0         = Inform how many B0 volumes your output DTI data should have."
+  echo "   Spatial resolution   = Choose a desirable final spatial resolution to your data. For instance, 2 mm."
+  echo ""
+}
 
+if [[ $# -eq 0 ]]; then
+  usage
+  exit
+elif [[ $# -lt 6 ]]; then
+  usage
+  exit
+fi
+
+INPUT_IMG=$1
+BVAL=$2
+BVEC=$3
+MATRIX=$4
+NUM_B0=$5
+RESOLUTION=$6
+
+
+
+echo ""
+echo "***** Phantom reconstruction *****"
+echo ""
+
+echo "    ==> Resampling the gold standard data with Spherical Harmonics..."
+matlab nohup -nodesktop -nodisplay -nosplash << EOF
+ addpath(genpath('/home/antonio/Documents/GitProjects/multishell/'));
+ addpath(genpath('/home/antonio/Documents/NBL/spherical_resamp/'));
+
+ multishell_resemp('$INPUT_IMG','$BVAL','$BVEC',$NUM_B0,60,8);
+
+EOF
+
+echo ""
 # Downsample to 2mm spatial resolution
-# flirt -in $INPUT_IMG -ref $INPUT_IMG -out ${INPUT_IMG}_2mm.nii -applyisoxfm $RESOLUTION
+echo "    ==> Downsampling the data to $RESOLUTION mm..."
+INPUT_IMG=`ls *_resamp.nii`
+flirt -in $INPUT_IMG -ref $INPUT_IMG -out tmp_dti_${RESOLUTION}mm.nii -applyisoxfm $RESOLUTION
 
 # Decompress the 2 mm image
- # gzip -d ${INPUT_IMG}_${RESOLUTION}mm.nii.gz
+ gzip -d tmp_dti_${RESOLUTION}mm.nii.gz
 
 # Resetting the input image variable
-INPUT_IMG=${INPUT_IMG}_${RESOLUTION}mm.nii
+INPUT_IMG=tmp_dti_${RESOLUTION}mm.nii
+
+# Reducing the b0 volumes
+fslroi $INPUT_IMG tmp_cropDTI $((18-$NUM_B0)) -1
+INPUT_IMG=tmp_cropDTI.nii.gz
 
 # Split the DTI volume in 3D volumes
-fslsplit ${INPUT_IMG}.nii tmp_
+echo "    ==> Spliting 4D DTI volume to a series of 3D volumes... "
+fslsplit ${INPUT_IMG}.nii.gz tmp_vol_
 
 # Cleaning eddy_correct matrix file to only matrix data
+echo "    ==> Formating matrix file..."
  cat $MATRIX | grep -v processing | grep -v Final | grep [[:alnum:]] > affine_4D.mat
 
 #MATLAB: split the affine_4D.mat file into several small affine matrix for each volumes
@@ -33,9 +81,11 @@ for i=1:4:rows
         count=count+1;
 end
 EOF
+
 echo ""
 
 #Apply transformation on each tmp volume
+echo "    ==> Applying affine transformation on each 3D volume"
 sizem=4
 count=1
 while read line; do
@@ -46,24 +96,25 @@ numMat=`echo $(($count/$sizem))`
 for (( i = 0; i < $numMat; i++ )); do
   if [[ $i -lt 10 ]]; then
     echo "--> Applying transformation in volume $i"
-    flirt -in tmp_000$i.nii.gz -ref tmp_000$i.nii.gz -init aff_$i.mat -out tmp_000${i}_aff.nii.gz -applyxfm
+    flirt -in tmp_vol_000$i.nii.gz -ref tmp_vol_000$i.nii.gz -init aff_$i.mat -out tmp_vol_000${i}_aff.nii.gz -applyxfm
   elif [[ $i -lt 100 && $i -ge 10 ]]; then
     echo "--> Applying transformation in volume $i"
-    flirt -in tmp_00$i.nii.gz -ref tmp_00$i.nii.gz -init aff_$i.mat -out tmp_00${i}_aff.nii.gz -applyxfm
+    flirt -in tmp_vol_00$i.nii.gz -ref tmp_vol_00$i.nii.gz -init aff_$i.mat -out tmp_vol_00${i}_aff.nii.gz -applyxfm
   elif [[ $i -lt 1000 && $i -ge 100 ]]; then
     echo "--> Applying transformation in volume $i"
-    flirt -in tmp_0$i.nii.gz -ref tmp_0$i.nii.gz -init aff_$i.mat -out tmp_0${i}_aff.nii.gz -applyxfm
+    flirt -in tmp_vol_0$i.nii.gz -ref tmp_vol_0$i.nii.gz -init aff_$i.mat -out tmp_vol_0${i}_aff.nii.gz -applyxfm
   fi
 done
 
 #Reconstructing the resampled+distorted volume
+echo "    ==> Reconstructing the final DTI volume with eddy current distortions..."
 for (( i = 0; i < $numMat; i++ )); do
   if [[ $i -lt 10 ]]; then
-    echo "tmp_000${i}_aff.nii.gz " >> tmp_filelist.txt
+    echo "tmp_vol_000${i}_aff.nii.gz " >> tmp_filelist.txt
   elif [[ $i -lt 100 && $i -ge 10 ]]; then
-    echo "tmp_00${i}_aff.nii.gz " >> tmp_filelist.txt
+    echo "tmp_vol_00${i}_aff.nii.gz " >> tmp_filelist.txt
   elif [[ $i -lt 1000 && $i -ge 100 ]]; then
-    echo "tmp_0${i}_aff.nii.gz " >> tmp_filelist.txt
+    echo "tmp_vol_0${i}_aff.nii.gz " >> tmp_filelist.txt
   fi
 done
 
@@ -71,6 +122,8 @@ done
  fslmerge -t dti_resamp_ec.nii.gz `cat tmp_filelist.txt`
 
 #Removing unncessary files
- rm aff* tmp*
+echo "    ==> Removing unncessary files (tmp, aff, brain_mask downsampled and resampled nii files)"
+ rm aff* tmp* brain_mask.nii *_2mm.nii *_resamp.nii
 
- # TODO FAZER REAJUSTE PARA QUE O VOLUME DTI TENHA O MEMSO NUMERO DE B0 - NESTE CASO TEM 18 MAS O SAGAPILOT TEM SO 6!!!!!!!!!!
+echo ""
+echo "***** Phantom reconstruction finished with success *****"
